7de395542ce3fde3bc22e0a9233bc9495895631d
dgarcia360 <dgarcia360@outlook.com>
2020-03-17 18:26:12 +0000

    Fix build

--- examples/GeneratePasswordProtectedSeedForRandomPassPhrase.ts
+++ examples/GeneratePasswordProtectedSeedForRandomPassPhrase.ts
@@ -2,4 +2,3 @@ import {MnemonicPassPhrase} from "../src/MnemonicPassPhrase";
 
 const mnemonic = MnemonicPassPhrase.createRandom();
 const secureSeedHex = mnemonic.toSeed('your-password');
-console.log(secureSeedHex)
